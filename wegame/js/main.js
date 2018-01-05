59979d6155a7b548c77852a21a14fa735668a559
NewYoung <657851574@qq.com>
2018-01-05 21:35:40 +0800

    threejs 魔方加入微信小游戏版本

---
+++ wegame/js/main.js
@@ -0,0 +1,67 @@
+import * as THREE from 'libs/three.js'
+
+let context   = canvas.getContext('webgl');
+
+/**
+ * 游戏主函数
+ */
+export default class Main {
+  constructor() {
+    this.initThree();
+    this.initCamera();
+    this.initScene();
+    this.initLight();
+    this.initObject();
+    this.render();
+  }
+
+  //初始化渲染器
+  initThree(){
+    this.context = context;
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.renderer = new THREE.WebGLRenderer({
+      context: this.context
+    });
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xFFFFFF, 1.0);
+  }
+
+  //初始化相机
+  initCamera(){
+    this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 1000);
+    this.camera.position.x = 0;
+    this.camera.position.y = 0;
+    this.camera.position.z = 600;
+    this.camera.up.x = 0;//正方向
+    this.camera.up.y = 1;
+    this.camera.up.z = 0;
+    this.camera.lookAt({
+      x: 0,
+      y: 0,
+      z: 0
+    });
+  }
+
+  //初始化场景
+  initScene(){
+    this.scene = new THREE.Scene();
+  }
+
+  //初始化光线
+  initLight() {
+    this.light = new THREE.AmbientLight(0xfefefe);
+    this.scene.add(this.light);
+  }
+
+  //初始化物体
+  initObject() {
+  }
+
+  //渲染
+  render() {
+    this.renderer.clear();
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.render.bind(this),canvas);
+  }
+}
