50b746648e8fc2d735feefa24711637e03e8a0fc
NewYoung <657851574@qq.com>
2018-11-05 22:07:09 +0800

    小修改

--- lesson/demo2/js/object/Rubik.js
+++ lesson/demo2/js/object/Rubik.js
@@ -7,7 +7,6 @@ const BasicParams = {
   z: 75,
   num: 3,
   len: 50,
-  defaultColor: '#666666',
   //右、左、上、下、前、后
   colors: ['#ff6b02', '#dd422f',
     '#ffffff', '#fdcd02',
@@ -15,7 +14,7 @@ const BasicParams = {
 };
 
 /**
- * 魔方
+ * 简易魔方
  * x、y、z 魔方正面左上角坐标
  * num 魔方单位方向上数量
  * len 魔方单位正方体宽高
@@ -25,20 +24,22 @@ function SimpleCube(x, y, z, num, len, colors) {
   var cubes = [];
   for (var i = 0; i < num; i++) {
     for (var j = 0; j < num * num; j++) {
+
       var myFaces = [];
       for (var k = 0; k < 6; k++) {
         myFaces[k] = faces(BasicParams.colors[k]);
       }
-      var cubegeo = new THREE.BoxGeometry(len, len, len);
+
       var materials = [];
       for (var k = 0; k < 6; k++) {
         var texture = new THREE.Texture(myFaces[k]);
         texture.needsUpdate = true;
-        materials.push(new THREE.MeshLambertMaterial({
-          map: texture
-        }));
+        materials.push(new THREE.MeshLambertMaterial({ map: texture}));
       }
+
+      var cubegeo = new THREE.BoxGeometry(len, len, len);
       var cube = new THREE.Mesh(cubegeo, materials);
+
       //假设整个魔方的中心在坐标系原点，推出每个小正方体的中心
       cube.position.x = (x + len / 2) + (j % 3) * len;
       cube.position.y = (y - len / 2) - parseInt(j / 3) * len;
@@ -72,17 +73,12 @@ function faces(rgbaColor) {
 }
 
 export default class Rubik {
-
   constructor(main) {
     this.main = main;
   }
-
-  /**
-   * 生成模型并加入到场景中
-   */
+  
   model() {
-    //生成魔方小正方体
-    this.cubes = SimpleCube(BasicParams.x, BasicParams.y, BasicParams.z, BasicParams.num, BasicParams.len, BasicParams.colors);
+    this.cubes = SimpleCube(BasicParams.x, BasicParams.y, BasicParams.z, BasicParams.num, BasicParams.len, BasicParams.colors);//生成魔方小正方体
     for (var i = 0; i < this.cubes.length; i++) {
       var item = this.cubes[i];
       this.main.scene.add(item);
