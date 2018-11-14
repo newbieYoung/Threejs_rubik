25e30b992def294b6fab03e05a1b4360b73cb880
NewYoung <657851574@qq.com>
2018-11-14 22:04:09 +0800

    正反视角 示例提交

---
+++ lesson/demo3/js/main.js
@@ -0,0 +1,97 @@
+import * as THREE from 'threejs/three.js'
+require('threejs/OrbitControls.js')
+import BasicRubik from 'object/Rubik.js'
+
+const Context = canvas.getContext('webgl');
+
+/**
+ * 游戏主函数
+ */
+export default class Main {
+  constructor() {
+
+    this.context = Context;//绘图上下文
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.devicePixelRatio = window.devicePixelRatio;
+    this.viewCenter = new THREE.Vector3(0, 0, 0);//原点
+    this.frontViewName = 'front-rubik';//正视图名称
+    this.endViewName = 'end-rubik';//反视图名称
+    this.minPercent = 0.25;//正反视图至少占25%区域
+
+    this.initRender();
+    this.initCamera();
+    this.initScene();
+    this.initLight();
+    this.initObject();
+    this.render();
+  }
+
+  /**
+   * 初始化渲染器
+   */
+  initRender() {
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      context: this.context
+    });
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xFFFFFF, 1.0);
+    canvas.width = this.width * this.devicePixelRatio;
+    canvas.height = this.height * this.devicePixelRatio;
+    this.renderer.setPixelRatio(this.devicePixelRatio);
+  }
+
+  /**
+   * 初始化相机
+   */
+  initCamera() {
+    this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 1000);
+    this.camera.position.set(0, 0, 300 / this.camera.aspect);
+    this.camera.up.set(0, 1, 0);//正方向
+    this.camera.lookAt(this.viewCenter);
+
+    //轨道视角控制器
+    this.orbitController = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+    this.orbitController.enableZoom = false;
+    this.orbitController.rotateSpeed = 2;
+    this.orbitController.target = this.viewCenter;//设置控制点
+  }
+
+  /**
+   * 初始化光线
+   */
+  initScene() {
+    this.scene = new THREE.Scene();
+  }
+
+  /**
+   * 初始化光线
+   */
+  initLight() {
+    this.light = new THREE.AmbientLight(0xfefefe);
+    this.scene.add(this.light);
+  }
+
+  /**
+   * 初始化物体
+   */
+  initObject() {
+    //正视角
+    this.frontRubik = new BasicRubik(this);
+    this.frontRubik.model(this.frontViewName);
+
+    //反视角
+    this.endRubik = new BasicRubik(this);
+    this.endRubik.model(this.endViewName);
+  }
+
+  /**
+   * 渲染
+   */
+  render() {
+    this.renderer.clear();
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.render.bind(this), canvas);
+  }
+}
---
+++ lesson/demo3/js/object/Rubik.js
@@ -0,0 +1,117 @@
+import * as THREE from '../threejs/three.js'
+
+//基础模型参数
+const BasicParams = {
+  x: 0,
+  y: 0,
+  z: 0,
+  num: 3,
+  len: 50,
+  //右、左、上、下、前、后
+  colors: ['#ff6b02', '#dd422f',
+    '#ffffff', '#fdcd02',
+    '#3d81f7', '#019d53']
+};
+
+/**
+ * 简易魔方
+ * x、y、z 魔方中心点坐标
+ * num 魔方阶数
+ * len 小方块宽高
+ * colors 魔方六面体颜色
+ */
+function SimpleCube(x, y, z, num, len, colors) {
+  //魔方左上角坐标
+  var leftUpX = x - num / 2 * len;
+  var leftUpY = y + num / 2 * len;
+  var leftUpZ = z + num / 2 * len;
+
+  var cubes = [];
+  for (var i = 0; i < num; i++) {
+    for (var j = 0; j < num * num; j++) {
+
+      var myFaces = [];
+      for (var k = 0; k < 6; k++) {
+        myFaces[k] = faces(BasicParams.colors[k]);
+      }
+
+      var materials = [];
+      for (var k = 0; k < 6; k++) {
+        var texture = new THREE.Texture(myFaces[k]);
+        texture.needsUpdate = true;
+        materials.push(new THREE.MeshLambertMaterial({ map: texture }));
+      }
+
+      var cubegeo = new THREE.BoxGeometry(len, len, len);
+      var cube = new THREE.Mesh(cubegeo, materials);
+
+      //依次计算各个小方块中心点坐标
+      cube.position.x = (leftUpX + len / 2) + (j % num) * len;
+      cube.position.y = (leftUpY - len / 2) - parseInt(j / num) * len;
+      cube.position.z = (leftUpZ - len / 2) - i * len;
+      cubes.push(cube)
+    }
+  }
+  return cubes;
+}
+
+/**
+ * 生成canvas素材
+ */
+function faces(rgbaColor) {
+  var canvas = document.createElement('canvas');
+  canvas.width = 256;
+  canvas.height = 256;
+  var context = canvas.getContext('2d');
+  //画一个宽高都是256的黑色正方形
+  context.fillStyle = 'rgba(0,0,0,1)';
+  context.fillRect(0, 0, 256, 256);
+  //在内部用某颜色的16px宽的线再画一个宽高为224的圆角正方形并用改颜色填充
+  context.rect(16, 16, 224, 224);
+  context.lineJoin = 'round';
+  context.lineWidth = 16;
+  context.fillStyle = rgbaColor;
+  context.strokeStyle = rgbaColor;
+  context.stroke();
+  context.fill();
+  return canvas;
+}
+
+export default class Rubik {
+  constructor(main) {
+    this.main = main;
+  }
+  
+  model(type) {
+    //创建魔方集合
+    this.group = new THREE.Group();
+    this.group.childType = type;
+
+    this.cubes = SimpleCube(BasicParams.x, BasicParams.y, BasicParams.z, BasicParams.num, BasicParams.len, BasicParams.colors);//生成魔方小正方体
+    for (var i = 0; i < this.cubes.length; i++) {
+      var item = this.cubes[i];
+
+      /**
+       * 小方块不再直接加入场景了；
+       * 而是先加入魔方集合，然后再把魔方集合加入场景。
+       */
+      //this.main.scene.add(item);
+      this.group.add(item);
+    }
+    this.main.scene.add(this.group);
+  }
+
+  /**
+   * 高度所占比例发生变化
+   */
+  resizeHeight(percent, transformTag) {
+    if (percent < this.main.minPercent) {
+      percent = this.main.minPercent;
+    }
+    if (percent > (1 - this.main.minPercent)) {
+      percent = 1 - this.main.minPercent;
+    }
+    this.group.scale.set(percent, percent, percent);
+    this.group.position.y = this.main.originHeight * (0.5 - percent / 2) * transformTag;
+  }
+}
