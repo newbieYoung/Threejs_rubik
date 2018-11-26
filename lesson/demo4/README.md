e1772f79b44b3e86245802084a418f68f661f783
NewYoung <657851574@qq.com>
2018-11-26 14:39:13 +0800

    转动魔方

---
+++ lesson/demo4/README.md
@@ -0,0 +1,16 @@
+## 转动魔方
+
+## 目录介绍
+```
+./adapter
+./images 
+./js
+├── threejs
+│   ├── three.js                           
+│   └── OrbitControls.js           
+├── object
+│   ├── Rubiks.js  
+│   └── TouchLine.js             
+└── main.js                                
+
+```
---
+++ lesson/demo4/js/main.js
@@ -0,0 +1,150 @@
+import * as THREE from 'threejs/three.js'
+import BasicRubik from 'object/Rubik.js'
+import TouchLine from 'object/TouchLine.js'
+
+const Context = canvas.getContext('webgl');
+
+/**
+ * 游戏主函数
+ */
+export default class Main {
+  constructor() {
+
+    this.context = Context;//绘图上下文
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.devicePixelRatio = window.devicePixelRatio;
+    this.viewCenter = new THREE.Vector3(0, 0, 0);//原点
+    this.frontViewName = 'front-rubik';//正视图名称
+    this.endViewName = 'end-rubik';//反视图名称
+    this.minPercent = 0.25;//正反视图至少占25%区域
+
+    this.initRender();
+    this.initCamera();
+    this.initScene();
+    this.initLight();
+    this.initObject();
+    this.initEvent();
+    this.render();
+  }
+
+  /**
+   * 初始化渲染器
+   */
+  initRender() {
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      context: this.context
+    });
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xFFFFFF, 1.0);
+    canvas.width = this.width * this.devicePixelRatio;
+    canvas.height = this.height * this.devicePixelRatio;
+    this.renderer.setPixelRatio(this.devicePixelRatio);
+  }
+
+  /**
+   * 初始化相机
+   */
+  initCamera() {
+    this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 1500);
+    this.camera.position.set(0, 0, 300 / this.camera.aspect);
+    this.camera.up.set(0, 1, 0);//正方向
+    this.camera.lookAt(this.viewCenter);
+
+    //透视投影相机视角为垂直视角，根据视角可以求出原点所在裁切面的高度，然后已知高度和宽高比可以计算出宽度
+    this.originHeight = Math.tan(22.5 / 180 * Math.PI) * this.camera.position.z * 2;
+    this.originWidth = this.originHeight * this.camera.aspect;
+  }
+
+  /**
+   * 初始化光线
+   */
+  initScene() {
+    this.scene = new THREE.Scene();
+  }
+
+  /**
+   * 初始化光线
+   */
+  initLight() {
+    this.light = new THREE.AmbientLight(0xfefefe);
+    this.scene.add(this.light);
+  }
+
+  /**
+   * 初始化物体
+   */
+  initObject() {
+    //正视角
+    this.frontRubik = new BasicRubik(this);
+    this.frontRubik.model(this.frontViewName);
+    this.frontRubik.resizeHeight(0.5, 1);
+
+    //反视角
+    this.endRubik = new BasicRubik(this);
+    this.endRubik.model(this.endViewName);
+    this.endRubik.resizeHeight(0.5, -1);
+
+    //滑动控制条
+    this.touchLine = new TouchLine(this);
+    this.rubikResize((1 - this.minPercent), this.minPercent);//默认正视图占85%区域，反视图占15%区域
+  }
+
+  /**
+   * 初始化事件
+   */
+  initEvent() {
+    wx.onTouchStart(this.touchStart.bind(this));
+    wx.onTouchMove(this.touchMove.bind(this));
+    wx.onTouchEnd(this.touchEnd.bind(this));
+  }
+
+  /**
+   * 触摸开始
+   */
+  touchStart(event){
+    var touch = event.touches[0];
+    this.startPoint = touch;
+    if (touch.clientY >= this.touchLine.screenRect.top && touch.clientY <= this.touchLine.screenRect.top + this.touchLine.screenRect.height) {
+      this.touchLine.enable();
+    }
+  }
+
+  /**
+   * 触摸移动
+   */
+  touchMove(event){
+    var touch = event.touches[0];
+    if (this.touchLine.isActive) {//滑动touchline
+      this.touchLine.move(touch.clientY);
+      var frontPercent = touch.clientY / window.innerHeight;
+      var endPercent = 1 - frontPercent;
+      this.rubikResize(frontPercent, endPercent);
+    }
+  }
+
+  /**
+   * 触摸结束
+   */
+  touchEnd(){
+    this.touchLine.disable();
+  }
+
+  /**
+   * 渲染
+   */
+  render() {
+    this.renderer.clear();
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.render.bind(this), canvas);
+  }
+
+  /**
+   * 正反魔方区域占比变化
+   */
+  rubikResize(frontPercent, endPercent) {
+    this.frontRubik.resizeHeight(frontPercent, 1);
+    this.endRubik.resizeHeight(endPercent, -1);
+  }
+}
